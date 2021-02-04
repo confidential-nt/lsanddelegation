73c2a734537910be89b9fa9c8157dffcc1bf34e6
confidential-nt <youyoy689@gmail.com>
2021-02-05 07:03:50 +0900

    finish one challenge

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ const TERM = "plate";
 const form = document.querySelector(".add-items");
 const plates = document.querySelector(".plates");
 const plateArray = JSON.parse(localStorage.getItem(TERM)) || [];
+const allChecker = document.querySelector("[id=all]");
+
+const allCheckObj = { text: "all", checked: false };
+
+function handleAllChecker(e) {
+  plateArray.forEach((plate) => {
+    if (e.target.checked) {
+      plate.selected = true;
+      allCheckObj.checked = true;
+    } else {
+      allCheckObj.checked = false;
+      plate.selected = false;
+    }
+    setLocalStorage();
+    drawPlates();
+  });
+}
 
 function handleChecking(e) {
   if (!e.target.matches("input")) return;
@@ -17,16 +34,19 @@ function makeArray(text) {
 }
 
 function setLocalStorage() {
+  localStorage.setItem("all", JSON.stringify(allCheckObj));
   localStorage.setItem(TERM, JSON.stringify(plateArray));
 }
 
 function drawPlates() {
+  const parsedCheckObj = JSON.parse(localStorage.getItem("all"));
+  if (parsedCheckObj) allChecker.checked = parsedCheckObj.checked;
   if (plateArray.length) {
     plates.innerHTML = plateArray
       .map((el, i) => {
         return `<li>
      <input type="checkbox" id="item${i}" data-id="${i}" ${
-          el.selected ? "checked" : ""
+          el.selected ? "checked" : parsedCheckObj.checked ? "checked" : ""
         }>
     <label for="item${i}">${el.text}</label></li>`;
       })
@@ -38,13 +58,17 @@ function addListItem(e) {
   e.preventDefault();
   const text = this.querySelector("[name=item]").value;
   makeArray(text);
-  drawPlates();
   setLocalStorage();
+  drawPlates();
+
   form.reset();
 }
 
 if (localStorage.getItem(TERM)) {
   drawPlates();
 }
+
 form.addEventListener("submit", addListItem);
 plates.addEventListener("click", handleChecking);
+allChecker.addEventListener("change", handleAllChecker);
+//챌린지 1. 모두 체크 2. 모두 안체크 3. 리스트 전부 리 렌더 하지말고 하나만
